88946c663d22e56f22ac4d4f2ac173ddd8219b7a
achiko12629 <20achi10@gmail.com>
2024-06-26 02:49:08 +0400

    first commit

---
+++ DAY 01/homework/homework.py
@@ -0,0 +1,17 @@
+
+introduction="გამარჯობათ_მე_ვარ"
+
+
+name= "აჩიკო"
+surname="უთრუგაშვილი"
+age="14"
+years="წლის"
+haight="სანტიმეტრი"
+live="და ვცხოვრობ მცხეთის რაიონ სოფელ აღდგომლიანთკარში"
+
+
+
+
+
+
+print(introduction+" "+name+" "+surname+" "+age+" "+" "+years+" "+"174"+" "+haight+" "+live)
