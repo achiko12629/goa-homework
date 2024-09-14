59dce08a702a7db49d1fb21e9dc9f1be287d2bc0
achiko12629 <20achi10@gmail.com>
2024-09-14 16:41:02 +0400

    commit

--- DAY 14/homework/idk_2.py
+++ DAY 14/homework/idk_2.py
@@ -1,33 +1,31 @@
-#  python
+#  Python
 
-# 2. while loop-ის მეშვეობით გამოიტანეთ ციფრები 10-დან 30-მდე.
-# 3. while loop-ის მეშვეობით გამოიტანეთ ციფრები 50-დან 10-მდე.
-# 4. while loop-ის მეშვეობით გამოიტანეთ 'goa best' 10-ჯერ.
-# 5. შემოიტანეთ რაიმე რიცხვი და გამოაცნობინეთ მომხმარებელს ეს რიცხვი.
+# 2. Input()-ის გამოყენებით მომხმარებელს შემოატანინეთ მისი სახელი, შემდეგ for loop-ით კი დაპრინტეთ "hello (მომხმარებლის სახელი)" 5 ჯერ.
+# 3. for loop-ის გამოყენებით დაპრინტეთ 20-მდე რიცხვები, თითოეული გაყოფილი 2-ზე.
+# 4. for loop-ის გამოყენებით დაპრინტეთ 15-მდე რიცხვები, თითოეული გამრავლებული 2-ზე.
+# 5. for loop-ის გამოყენებით დაპრინტეთ 10-მდე რიცხვები, თითოეულის კვადრატი.
+# 6. for loop-ის გამოყენებით დაპრინტეთ 10-მდე არსებული ყველა რიცხვის ჯამი, ეს ჯამი უნდა შეინახოს for loop-ის გარეთ შექმნილ ცვლად sum-ში.
+
+#1
+name = input("enter your name")
+for i in range(3):
+    print(f"hello {name}")
 
 #2
-age=10
-while age <= 30:
-    print(age)
-    age=age+1
+for i in range(20):
+    print(i/2)
 
 #3
-sam= 50
-while sam >= 10:
-    print(sam)
-    sam=sam - 1
+for i in range(20):
+    print(i * 2)
 
 #4
-goa = 10
-while goa > 0:
-    print("goa best")
-    goa= goa - 1
-
+for i in range(10):
+    print(i * i)
 
 #5
-idk =int( input("enter a nomber from 1 to 10 :"))
-while idk != 4:
-    print("your so close try agin ")
-    idk =int( input("enter a nomber from 1 to 10 :"))
-else :
-    print("you correctt you win :)))))) ")
+
+sum = 0
+for i in range(1, 11):
+    sum += i 
+print(sum)
